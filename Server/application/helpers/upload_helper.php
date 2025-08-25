4617d3e37c241dedab63f0dcc499f7c14670ec7a
Hajatiana-0407 <hajatianarasolondraibe0407@gmail.com>
2025-08-25 21:00:09 +0300

    Fix: debug upload helper

--- Server/application/helpers/upload_helper.php
+++ Server/application/helpers/upload_helper.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 if (!function_exists('upload_file')) {
     /**
@@ -11,15 +11,16 @@ if (!function_exists('upload_file')) {
      * @param int $max_size Taille max (KB)
      * @return array ['success' => bool, 'file_name' => string, 'error' => string]
      */
-    function upload_file($field_name, $upload_path, $allowed_types = ['jpg','jpeg','png','gif'], $max_size = 4096)
+    function upload_file($field_name, $upload_path, $allowed_types = ['jpg', 'jpeg', 'png', 'gif'], $max_size = 4096)
     {
-        $CI =& get_instance();
+        $CI = &get_instance();
 
         // Config upload
-        $config['upload_path']   = $upload_path;
+        $config['upload_path']   = FCPATH .  $upload_path;
         $config['allowed_types'] = implode('|', $allowed_types);
         $config['max_size']      = $max_size;
-        $config['file_name']     = uniqid() . '_' . time();
+        $ext = pathinfo($_FILES[$field_name]['name'], PATHINFO_EXTENSION);
+        $config['file_name']     = uniqid() . '_' . time() . '.' . $ext;
 
         $CI->load->library('upload', $config);
 
@@ -33,6 +34,7 @@ if (!function_exists('upload_file')) {
             return [
                 'success'   => true,
                 'file_name' => $uploadData['file_name'],
+                'ext' => $uploadData['file_ext'],
                 'error'     => null
             ];
         }
