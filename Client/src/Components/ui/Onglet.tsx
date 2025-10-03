96d035838d5596730b03a40a417373a91db0693d
Hajatiana-0407 <hajatianarasolondraibe0407@gmail.com>
2025-10-03 14:32:27 +0300

    Style ( Onglet ) : Add more style for the active and not actif onglet

--- Client/src/Components/ui/Onglet.tsx
+++ Client/src/Components/ui/Onglet.tsx
@@ -21,7 +21,7 @@ const Onglet: React.FC<OngletPropsType> = ({ onlgets }) => {
                             onClick={() => setActiveTab(ongle.key.toLowerCase())}
                             className={`py-2 relative cursor-pointer px-2 rounded-t  font-medium text-sm  ${activeTab === ongle.key.toLowerCase()
                                 ? 'border-blue-300 border border-b-0  text-blue-600'
-                                : 'border-gray-200 border border-b-0 text-gray-500 hover:text-gray-700 hover:border-gray-300'
+                                : 'border-gray-200 bg-gray-50 border border-b-0 text-gray-500 hover:text-gray-700 hover:border-gray-300'
                                 }`}
                         >
                             {ongle.key}
