b94177157c063495d17b673c6b812cdfaf3ac2ae
Hajatiana-0407 <hajatianarasolondraibe0407@gmail.com>
2025-10-09 15:52:20 +0300

    feat( environement front ) : Add .env file for the environement

--- Client/src/Utils/api.ts
+++ Client/src/Utils/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    // baseURL: 'http://localhost/Mirva-School-Management/Server/api',
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
     }
