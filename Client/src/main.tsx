43b31b0f56187a2e077939bbea00c6321e60db2a
Hajatiana-0407 <hajatianarasolondraibe0407@gmail.com>
2025-10-14 10:25:26 +0300

    fix ( token ) : treat the expired token case

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -5,6 +5,9 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './Redux/store.ts';
+import { setupInterceptors } from './Utils/api.ts';
+
+setupInterceptors(store);
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
